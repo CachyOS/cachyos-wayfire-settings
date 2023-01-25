fecd76cdbc0ea3ec2e84581419898ec1492636b5
Vasiliy Stelmachenok <ventureo@yandex.ru>
2023-01-25 15:15:12 +0300

    Export variables to run on NVIDIA

--- etc/profile.d/wayfire.sh
+++ etc/profile.d/wayfire.sh
@@ -1,3 +1,6 @@
-if lspci -k | grep "Kernel driver in use" | grep nouveau; then 
-	export WLR_NO_HARDWARE_CURSORS=1
+if [ -d /sys/module/nvidia ]; then
+    export WLR_NO_HARDWARE_CURSORS=1
+    export GBM_BACKEND=nvidia-drm
+    export EGL_PLATFORM=wayland
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
 fi
